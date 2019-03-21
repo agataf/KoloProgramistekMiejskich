364481ea7d3ef30d7c31d707dbc5c215cf8d01b9
agataf <agata@foryciarz.com>
2019-03-21 21:22:09 +0100

    usun

--- zadania1/poziom1/pole.py
+++
@@ -1,8 +0,0 @@
-'''
-Napisz program który prosi użytkownika o promien okregu i oblicza jego pole.
-
-Przyklad:
-jaki promien ma twoj okrag?
-> 5
-Twoj okrag ma pole 75.39
-'''
--- zadania1/poziom2/rewers.py
+++
@@ -1,8 +0,0 @@
-'''
-Przyjmij od użytkowniczki 5 słów, zwróć je w odwróconej kolejności.
-
-Podaj 5 słów
-> Kodowanie jest wlasciwie calkiem latwe
-latwe calkiem wlasciwie jest kodowanie
-
-'''
